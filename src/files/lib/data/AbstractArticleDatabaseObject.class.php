4099507d90ac3b3b073aaba2fa6727f788db620c
Jens Krumsieck <jenso@codequake.de>
2015-10-28 18:38:18 +0100

    codestyle

--- src/files/lib/data/AbstractArticleDatabaseObject.class.php
+++ src/files/lib/data/AbstractArticleDatabaseObject.class.php
@@ -26,260 +26,261 @@ use wcf\util\StringUtil;
 
 abstract class AbstractArticleDatabaseObject extends DatabaseObject implements IRouteController, IMessage, IBreadcrumbProvider
 {
-	/**
-	 * php class for categories
-	 * @var string
-	 */
-	protected static $categoryBasicClass = '';
+    /**
+     * php class for categories
+     * @var string
+     */
+    protected static $categoryBasicClass = '';
 
-	/**
-	 * categoryIDs article is connected to
-	 * @var array<int>
-	 */
-	protected $categoryIDs = array();
+    /**
+     * categoryIDs article is connected to
+     * @var array<int>
+     */
+    protected $categoryIDs = array();
 
-	/**
-	 * @var string
-	 */
-	protected static $objectType = '';
+    /**
+     * @var string
+     */
+    protected static $objectType = '';
 
-	/** 
-	 * main controller for viewing objects of this class
-	 * @var	string 
-	 */ 
-	protected static $objectViewController = ''; 
+    /** 
+     * main controller for viewing objects of this class
+     * @var    string 
+     */ 
+    protected static $objectViewController = '';
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function __toString()
-	{
-		return $this->getFormattedMessage();
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        return $this->getFormattedMessage();
+    }
 
-	/**
-	 * check whether user can add new object
-	 * @return bool
-	 */
-	public function canAdd()
-	{
-		return false;
-	}
+    /**
+     * check whether user can add new object
+     * @return bool
+     */
+    public function canAdd()
+    {
+        return false;
+    }
 
-	/**
-	 * check whether user can moderate current object
-	 * @return bool
-	 */
-	public function canModerate()
-	{
-		return false;
-	}
+    /**
+     * check whether user can moderate current object
+     * @return bool
+     */
+    public function canModerate()
+    {
+        return false;
+    }
 
-	/**
-	 * @check whether user can read current object
-	 * @return bool
-	 */
-	public function canRead()
-	{
-		return false;
-	}
+    /**
+     * @check whether user can read current object
+     * @return bool
+     */
+    public function canRead()
+    {
+        return false;
+    }
 
-	/**
-	 * @return \wcf\data\attachment\GroupedAttachmentList
-	 */
+    /**
+     * @return \wcf\data\attachment\GroupedAttachmentList
+     */
+    public function getAttachments()
+    {
+        if (MODULE_ATTACHMENT && $this->attachments) {
+            $attachmentList = new GroupedAttachmentList(self::$objectType);
+            $attachmentList->getConditionBuilder()->add('attachment.objectID IN (?)', array($this->{static::getDatabaseTableIndexName()}));
+            $attachmentList->readObjects();
+            //add permissions!
+            $attachmentList->setPermissions(array(
+                'canDownload' => '',
+                'canViewPreview' => ''
+            ));
+            AttachmentBBCode::setAttachmentList($attachmentList);
 
-	public function getAttachments()
-	{
-		if (MODULE_ATTACHMENT && $this->attachments) {
-			$attachmentList = new GroupedAttachmentList(self::$objectType);
-			$attachmentList->getConditionBuilder()->add('attachment.objectID IN (?)', array($this->{static::getDatabaseTableIndexName()}));
-			$attachmentList->readObjects();
-			//add permissions!
-			$attachmentList->setPermissions(array(
-				'canDownload' => '',
-				'canViewPreview' => ''
-			));
-			AttachmentBBCode::setAttachmentList($attachmentList);
+            return $attachmentList;
+        }
 
-			return $attachmentList;
-		}
-		return;
-	}
+        return;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getBreadcrumb()
-	{
-		return new Breadcrumb($this->subject, $this->getLink());
-	}
-	/** 
-	 * @return array<self::$categoryBasicClass> 
-	 */ 
-	public function getCategories()
-	{
-		$classParts = explode('\\', get_called_class());
-		$articleType = explode('.', self::$objectType);
-		if ($this->categories === null) {
-			$this->categories = array();
+    /**
+     * {@inheritdoc}
+     */
+    public function getBreadcrumb()
+    {
+        return new Breadcrumb($this->subject, $this->getLink());
+    }
 
-			 if (0 !== count($this->categoryIDs)) {
-				foreach ($this->categoryIDs as $categoryID) {
-					$this->categories[$categoryID] = new self::$categoryBasicClass(CategoryHandler::getInstance()->getCategory($categoryID));
-				} 
-			} else { 
-				$sql = ' 
-					SELECT categoryID 
-					FROM '.$classParts[0].WCF_N.'_'.end($articleType).'_to_category 
-					WHERE '.static::getDatabaseTableIndexName().' = ?';
-					$statement = WCF::getDB()->prepareStatement($sql);
-					$statement->execute(array($this->{static::getDatabaseTableIndexName()}));
+    /**
+     * @return array<self::$categoryBasicClass>
+     */
+    public function getCategories()
+    {
+        $classParts = explode('\\', get_called_class());
+        $articleType = explode('.', self::$objectType);
+        if ($this->categories === null) {
+            $this->categories = array();
 
-					while ($row = $statement->fetchArray()) {
-						$this->categories[$row['categoryID']] = new self::$categoryBasicClass(CategoryHandler::getInstance()->getCategory($row['categoryID']));
-					}
-				}
-			}
+             if (0 !== count($this->categoryIDs)) {
+                foreach ($this->categoryIDs as $categoryID) {
+                    $this->categories[$categoryID] = new self::$categoryBasicClass(CategoryHandler::getInstance()->getCategory($categoryID));
+                    }
+            } else {
+                $sql = '
+                    SELECT categoryID 
+                    FROM '.$classParts[0].WCF_N.'_'.end($articleType).'_to_category
+                    WHERE '.static::getDatabaseTableIndexName().' = ?';
+                    $statement = WCF::getDB()->prepareStatement($sql);
+                    $statement->execute(array($this->{static::getDatabaseTableIndexName()}));
 
-			return $this->categories;
-		} 
+                    while ($row = $statement->fetchArray()) {
+                        $this->categories[$row['categoryID']] = new self::$categoryBasicClass(CategoryHandler::getInstance()->getCategory($row['categoryID']));
+                    }
+                }
+            }
+
+            return $this->categories;
+        }
 
 
-	/**
-	 * @return int[]
-	 */
-	public function getCategoryIDs()
-	{
-		return $this->categoryIDs;
-	}
+    /**
+     * @return int[]
+     */
+    public function getCategoryIDs()
+    {
+        return $this->categoryIDs;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getExcerpt($maxLength = 255)
-	{
-		return StringUtil::truncateHTML($this->getSimplifiedFormattedMessage(), $maxLength);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getExcerpt($maxLength = 255)
+    {
+        return StringUtil::truncateHTML($this->getSimplifiedFormattedMessage(), $maxLength);
+    }
 
-	/** 
-	 *	{@inheritdoc}
-	 */
-	public function getFormattedMessage()
-	{
-		AttachmentBBCode::setObjectID($this->{static::getDatabaseTableIndexName()});
-		MessageParser::getInstance()->setOutputType('text/html');
-		return MessageParser::getInstance()->parse($this->getMessage(), $this->enableSmilies, $this->enableHtml, $this->enableBBCodes);
-	}
+    /** 
+     *    {@inheritdoc}
+     */
+    public function getFormattedMessage()
+    {
+        AttachmentBBCode::setObjectID($this->{static::getDatabaseTableIndexName()});
+        MessageParser::getInstance()->setOutputType('text/html');
+        return MessageParser::getInstance()->parse($this->getMessage(), $this->enableSmilies, $this->enableHtml, $this->enableBBCodes);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getLanguage()
-	{
-		if ($this->languageID) { 
-			 return LanguageFactory::getInstance()->getLanguage($this->languageID); 
-		 } 
-		return; 
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getLanguage()
+    {
+        if ($this->languageID) {
+             return LanguageFactory::getInstance()->getLanguage($this->languageID);
+         }
+        return;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getLanguageIcon()
-	{
-		return '<img src="'.$this->getLanguage()->getIconPath().'" alt="" title="'.$this->getLanguage().'" class="jsTooltip iconFlag" />';
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getLanguageIcon()
+    {
+        return '<img src="'.$this->getLanguage()->getIconPath().'" alt="" title="'.$this->getLanguage().'" class="jsTooltip iconFlag" />';
+    }
 
-	/** 
-	 * {@inheritdoc} 
-	 * 
-	 * @param bool $appendSession 
-	 */ 
-	public function getLink($appendSession = true) 
-	{
-		$classParts = explode('\\', get_called_class());
-		return LinkHandler::getInstance()->getLink(self::$objectViewController, array( 
-			'application' => $classParts[0], 
-			'object' => $this, 
-			'appendSession' => $appendSession, 
-			'forceFrontend' => true, 
-		)); 
-	}
+    /** 
+     * {@inheritdoc} 
+     * 
+     * @param bool $appendSession 
+     */ 
+    public function getLink($appendSession = true)
+    {
+        $classParts = explode('\\', get_called_class());
+        return LinkHandler::getInstance()->getLink(self::$objectViewController, array(
+            'application' => $classParts[0],
+            'object' => $this,
+            'appendSession' => $appendSession,
+            'forceFrontend' => true,
+        ));
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getMessage()
-	{
-		return $this->message;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
 
-	/**
-	 *	{@inheritdoc}
-	 */
-	public function getSimplifiedFormattedMessage()
-	{
-		MessageParser::getInstance()->setOutputType('text/simplified-html');
-		return MessageParser::getInstance()->parse($this->getMessage(), $this->enableSmilies, $this->enableHtml, $this->enableBBCodes);
-	}
+    /**
+     *    {@inheritdoc}
+     */
+    public function getSimplifiedFormattedMessage()
+    {
+        MessageParser::getInstance()->setOutputType('text/simplified-html');
+        return MessageParser::getInstance()->parse($this->getMessage(), $this->enableSmilies, $this->enableHtml, $this->enableBBCodes);
+    }
 
-	/**
-	 * @return array<\wcf\data\tag\Tag>
-	 */
-	public function getTags()
-	{
-		$tags = TagEngine::getInstance()->getObjectTags(static::objectType, 
-														$this->{static::getDatabaseTableIndexName()}, 
-														array(($this->languageID === null ? LanguageFactory::getInstance()->getDefaultLanguageID() : $this->languageID))
-														);
-		return $tags;
-	}
+    /**
+     * @return array<\wcf\data\tag\Tag>
+     */
+    public function getTags()
+    {
+        $tags = TagEngine::getInstance()->getObjectTags(static::objectType, 
+                                                        $this->{static::getDatabaseTableIndexName()}, 
+                                                        array(($this->languageID === null ? LanguageFactory::getInstance()->getDefaultLanguageID() : $this->languageID))
+                                                        );
+        return $tags;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getTime()
-	{
-		return $this->time;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getTitle()
-	{
-		return $this->title;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getUserID()
-	{
-		return $this->userID;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getUserID()
+    {
+        return $this->userID;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getUsername()
-	{
-		return $this->username;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
 
-	/**
-	 * @param int $categoryID
-	 */
-	public function setCategoryID($categoryID)
-	{
-		$this->categoryIDs[] = $categoryID;
-	}
+    /**
+     * @param int $categoryID
+     */
+    public function setCategoryID($categoryID)
+    {
+        $this->categoryIDs[] = $categoryID;
+    }
 
-	/**
-	 * @param array<int> $categoryIDs
-	 */
-	public function setCategoryIDs(array $categoryIDs)
-	{
-		$this->categoryIDs = $categoryIDs;
-	}
+    /**
+     * @param array<int> $categoryIDs
+     */
+    public function setCategoryIDs(array $categoryIDs)
+    {
+        $this->categoryIDs = $categoryIDs;
+    }
 }
