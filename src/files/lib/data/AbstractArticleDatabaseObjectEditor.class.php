3c72bb0d347d00fdbc4968fed8d202123e0e4610
Jens Krumsieck <jenso@codequake.de>
2015-10-28 19:39:41 +0100

    abstract editor class

---
+++ src/files/lib/data/AbstractArticleDatabaseObjectEditor.class.php
@@ -0,0 +1,48 @@
+<?php
+namespace wcf\data;
+
+use wcf\system\WCF;
+/**
+ * Abstract editor class for all article based database objects.
+ *
+ * @author Jens Krumsieck
+ * @copyright 2013-2015 codeQuake
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package de.codequake.core.article
+ */
+abstract class AbstractArticleDatabaseObjectEditor extends DatabaseObjectEditor
+{
+    /**
+     * @param int[] categoryIDs
+     */
+    public function updateCategoryIDs(array $categoryIDs = array())
+    {
+        //remove old
+        $classParts = explode('\\', get_called_class());
+        $baseClass = static::getBaseClass();
+        $articleType = explode('.', $baseClass::$objectType);
+        $sql = '
+            DELETE FROM '.$classParts[0].WCF_N.'_'.$articleType.'_to_category
+            WHERE '.static::getDatabaseTableIndexName().' = ?';
+        $statement = WCF::getDB()->prepareStatement($sql);
+        $statement->execute(array($this->{static::getDatabaseTableIndexName()}));
+
+        //assign new
+        if (count($categoryIDs) !== 0) {
+            WCF::getDB()->beginTransaction();
+
+            $sql = '
+                INSERT INTO '.$classParts[0].WCF_N.'_'.$articleType.'_to_category
+                    (categoryID, '.static::getDatabaseTableIndexName().')
+                VALUES (?,?)';
+            $statement = WCF::getDB()->prepareStatement($sql);
+            foreach ($categoryIDs as $categoryID) {
+                $statement->execute(array(
+                    $categoryID,
+                    $this->{static::getDatabaseTableIndexName()},
+                ));
+            }
+            WCF::getDB()->commitTransaction();
+        }
+    }
+}
